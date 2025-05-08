657d9677356c04e4df37e008cd908d7fcf041f27
Markd-project <markd1885929@gmail.com>
2025-05-08 00:29:56 -0400

    Update checkinput.py

--- checkinput.py
+++ checkinput.py
@@ -4,3 +4,18 @@
 #The function will continue to prompt the user to enter an integer until a proper integer is entered.
 #The most direct way of doing this would be using a try block, which has not been covered yet. You will need to research this.
 #Write supporting code to call the function, and then display the number that was entered.
+
+def CheckInput():
+     while True:
+        try:
+                x = int(input("Please enter a number: "))
+                if type(x) == int:
+                        print(f"The number you entered was {x}")
+                        break
+        except:
+              print("That was not a number, please try again!")
+        
+
+
+
+CheckInput()
